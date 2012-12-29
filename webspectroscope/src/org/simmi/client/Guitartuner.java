524debeef1b8991b6f4a74b23679746e7d6c8e32
Sigmar Stefansson <sigmarkarl@gmail.com>
2012-12-29 14:46:12 +0000

    e

---
+++ webspectroscope/src/org/simmi/client/Guitartuner.java
@@ -0,0 +1,11 @@
+package org.simmi.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Guitartuner implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		
+	}
+}
