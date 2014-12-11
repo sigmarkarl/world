bc160ca7be5ab991396f2e1b399f973e19521fa1
Sigmar Stefansson <sigmarkarl@gmail.com>
2014-12-11 17:52:59 +0000

    ten

--- javafasta/src/org/simmi/unsigned/FlxReader.java
+++ javafasta/src/org/simmi/unsigned/FlxReader.java
@@ -3,6 +3,7 @@ package org.simmi.unsigned;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -25,26 +26,117 @@ public class FlxReader {
 		return ret;
 	}
 	
-	public static String type = "islandicus3838_30";
+	public static String tengi( Map<String,String> mog, String c2, int recurcount ) {
+		String nstuff = "";
+		if( recurcount < 5 ) {
+			for( String m : mog.keySet() ) {
+				String q1 = mog.get(m);
+				
+				int li = m.lastIndexOf('_');
+				String c3 = m.substring(0,li);
+				if( m.substring( li ).equals("_5'") ) {
+					Map<String,String> ss = mm.get(c3+"_3'");
+					if( ss != null ) {
+						if( ss.containsKey(c2+"_5'") ) {
+							String q2 = ss.get(c2+"_5'");
+							
+							if( nstuff.length() == 0 ) nstuff += "("+q1+")"+m+"("+q2+")";
+							else nstuff += " ("+q1+")"+m+"("+q2+")";
+							//break;
+						} else {
+							String st = tengi( ss, c2, recurcount+1 );
+							if( st.length() > 0 ) {
+								//String q2 = ss.get(c2+"_5'");
+								if( nstuff.length() == 0 ) nstuff += "("+q1+")"+m;
+								else nstuff += " ("+q1+")"+m;
+								nstuff += st;
+							}
+						}
+					}
+				} else {
+					Map<String,String> ss = mm.get(c3+"_5'");
+					if( ss != null ) {
+						if( ss.containsKey(c2+"_5'") ) {
+							String q2 = ss.get(c2+"_5'");
+							
+							if( nstuff.length() == 0 ) nstuff += "("+q1+")"+m+"("+q2+")";
+							else nstuff += " ("+q1+")"+m+"("+q2+")";
+							//break;
+						} else {
+							String st = tengi( ss, c2, recurcount+1 );
+							if( st.length() > 0 ) {
+								if( nstuff.length() == 0 ) nstuff += "("+q1+")"+m;
+								else nstuff += " ("+q1+")"+m;
+								nstuff += st;
+							}
+						}
+					}
+				}
+			}
+		}
+		return nstuff;
+	}
+	
+	/*
+	 * if( firstofnew != null ) {
+						int i = lastW[5].indexOf('0');
+						while( lastW[5].charAt(++i) == '0' ) ;
+						String c1 = lastW[5].substring(i);
+						
+						i = firstofnew[5].indexOf('0');
+						while( firstofnew[5].charAt(++i) == '0' ) ;
+						String c2 = firstofnew[5].substring(i);
+						
+						String nstuff = null;
+						if( mm.containsKey(c1+"_3'") ) {
+							Map<String,String> mog = mm.get(c1+"_3'");
+							nstuff = tengi( mog, c2, 0 );
+							if( mog.containsKey(c2+"_5'") ) {
+								String q = mog.get(c2+"_5'");
+								if( nstuff.length() == 0 ) nstuff = "directconnect("+q+")";
+								else nstuff += " directconnect("+q+")";
+							}
+						}
+						
+						if( nstuff != null && nstuff.length() > 0 ) {
+							System.out.println( nstuff );
+							String[] sp = nstuff.split(" ");
+							String nsplit = sp[sp.length-1];
+							int f = nsplit.indexOf(')');
+							int n = nsplit.indexOf('_',f+1);
+							if( n != -1 ) {
+								String sub = nsplit.substring(f+1, n);
+								String ctgname = sub.length() == 1 ? "contig0000"+sub : sub.length() == 2 ? "contig000"+sub : "contig00"+sub;
+								seq = mseq.get(ctgname);
+								touch.add( ctgname );
+								if( seq != null ) {
+									boolean rev = nsplit.charAt(n+1) == '3' ? true : false;
+									Sequence nseq = new Sequence( seq, rev );
+									nseq.setName( ctgname+"_"+(rev?"rev":"frw"));
+									//nseq.group = lastN[0];
+									serifier.addSequence( nseq );
+								}
+							}
+						}
+					}
+					//System.out.println( join(split) );
+	 */
+	
+	public static Map<String,Map<String,String>>	mm = new HashMap<String,Map<String,String>>();
+	
+	public static String home = "/home/sigmar/";
+	public static String type = "brockianus338_v30_minoverlap";
+	public static String add = "/assembly/";
 	public static void main(String[] args) {
 		try {
 			Map<String,Sequence>	mseq = new HashMap<String,Sequence>();
-			FileReader fr = new FileReader("/Users/sigmar/thermus_close/"+type+"/454ScaffoldContigs.fna");
-			BufferedReader	br = new BufferedReader( fr );
-			String line = br.readLine();
-			Sequence seq = null;
-			while( line != null ) {
-				if( line.startsWith(">") ) {
-					String name = line.substring(1);
-					name = name.substring(0,name.indexOf(' '));
-					seq = new Sequence( name, mseq );
-				} else {
-					seq.append(line);
-				}
-				line = br.readLine();
-			}
 			
-			fr = new FileReader("/Users/sigmar/thermus_close/"+type+"/454AllContigs.fna");
+			FileReader fr;
+			BufferedReader br;
+			String line;
+			Sequence seq;
+			
+			fr = new FileReader(home+type+add+"/454ScaffoldContigs.fna");
 			br = new BufferedReader( fr );
 			line = br.readLine();
 			seq = null;
@@ -58,10 +150,25 @@ public class FlxReader {
 				}
 				line = br.readLine();
 			}
+			fr.close();
 			
-			Map<String,Map<String,String>>	mm = new HashMap<String,Map<String,String>>();
+			fr = new FileReader(home+type+add+"454AllContigs.fna");
+			br = new BufferedReader( fr );
+			line = br.readLine();
+			seq = null;
+			while( line != null ) {
+				if( line.startsWith(">") ) {
+					String name = line.substring(1);
+					name = name.substring(0,name.indexOf(' '));
+					seq = new Sequence( name, mseq );
+				} else {
+					seq.append(line);
+				}
+				line = br.readLine();
+			}
+			fr.close();
 			
-			fr = new FileReader("/Users/sigmar/thermus_close/"+type+"/454ContigGraph.txt");
+			fr = new FileReader(home+type+add+"454ContigGraph.txt");
 			br = new BufferedReader( fr );
 			line = br.readLine();
 			while( line != null ) {
@@ -91,18 +198,26 @@ public class FlxReader {
 			}
 			br.close();
 			
+			int plasm = 1;
+			FileWriter fw = new FileWriter( home + type + ".fna" );
+			Sequence cseq = new Sequence("chromosome", null);
+			
 			Set<String>	touch = new HashSet<String>();
 			Serifier	serifier = new Serifier();
 			
-			fr = new FileReader("/Users/sigmar/thermus_close/"+type+"/454Scaffolds.txt");
+			fr = new FileReader(home+type+add+"454ContigScaffolds.txt");
 			br = new BufferedReader( fr );
 			
 			String[] lastW = null;
 			String[] lastN = null;
+			String[] firstofnew = null;
+			
+			int next = 1;
 			
 			line = br.readLine();
 			while( line != null ) {
 				String[] split = line.split("[\t ]+");
+				String scaff = split[0];
 				if( split[4].equals("W") ) {
 					if( lastW != null ) {
 						System.out.println( join(lastW) );
@@ -111,10 +226,16 @@ public class FlxReader {
 						seq.group = lastW[0];
 						if( seq != null ) serifier.addSequence( seq );
 					}
+					//boolean newscaff = !lastW[0].equals(split[0]);
 					if( lastN != null ) {
 						int i = lastW[5].indexOf('0');
 						while( lastW[5].charAt(++i) == '0' ) ;
 						String c1 = lastW[5].substring(i);
+						
+						i = lastW[0].indexOf('0');
+						String sctg = "sctg_"+lastW[0].substring(i+1)+"_000"+next++;
+						Sequence sctgseq = mseq.get(sctg);
+						cseq.append( sctgseq.sb );
 						
 						i = split[5].indexOf('0');
 						while( split[5].charAt(++i) == '0' ) ;
@@ -143,54 +264,143 @@ public class FlxReader {
 						}*/
 						if( mm.containsKey(c1+"_3'") ) {
 							Map<String,String> mog = mm.get(c1+"_3'");
-							for( String m : mog.keySet() ) {
-								String q1 = mog.get(m);
-								
-								int li = m.lastIndexOf('_');
-								String c3 = m.substring(0,li);
-								if( m.substring( li ).equals("_5'") ) {
-									Map<String,String> ss = mm.get(c3+"_3'");
-									if( ss != null && ss.containsKey(c2+"_5'") ) {
-										String q2 = ss.get(c2+"_5'");
-										
-										if( nstuff == null ) nstuff = "("+q1+")"+m+"("+q2+")";
-										else nstuff += " ("+q1+")"+m+"("+q2+")";
-										//break;
-									}
-								} else {
-									Map<String,String> ss = mm.get(c3+"_5'");
-									if( ss != null && ss.containsKey(c2+"_5'") ) {
-										String q2 = ss.get(c2+"_5'");
-										
-										if( nstuff == null ) nstuff = "("+q1+")"+m+"("+q2+")";
-										else nstuff += " ("+q1+")"+m+"("+q2+")";
-										//break;
-									}
-								}
+							nstuff = tengi( mog, c2, 0 );
+							if( mog.containsKey(c2+"_5'") ) {
+								String q = mog.get(c2+"_5'");
+								if( nstuff.length() == 0 ) nstuff = "directconnect("+q+")";
+								else nstuff += " directconnect("+q+")";
 							}
 						}
 						
-						if( nstuff != null ) {
+						if( nstuff != null && nstuff.length() > 0 ) {
+							String[] nssplit = nstuff.split(" ");
+							int maxmin = 1000;
+							int m = 0;
+							String store = "";
+							for( String ns : nssplit ) {
+								int l = ns.indexOf('(');
+								while( l != -1 ) {
+									int k = ns.indexOf(')',l+1);
+									
+									String nstr = ns.substring(l+1,k);
+									int n = Integer.parseInt( nstr );
+									maxmin = Math.min(maxmin, n);
+									
+									l = ns.indexOf('(',k+1);
+								}
+								
+								if( maxmin > m ) {
+									store = ns;
+									m = maxmin;
+								}
+							}
+							nstuff = store;
+							
 							System.out.println( nstuff );
 							String[] sp = nstuff.split(" ");
 							String nsplit = sp[sp.length-1];
 							int f = nsplit.indexOf(')');
 							int n = nsplit.indexOf('_',f+1);
-							String sub = nsplit.substring(f+1, n);
-							String ctgname = sub.length() == 1 ? "contig0000"+sub : sub.length() == 2 ? "contig000"+sub : "contig00"+sub;
-							seq = mseq.get(ctgname);
-							touch.add( ctgname );
-							if( seq != null ) {
-								boolean rev = nsplit.charAt(n+1) == '3' ? true : false;
-								Sequence nseq = new Sequence( seq, rev );
-								nseq.setName( ctgname+"_"+(rev?"rev":"frw"));
-								nseq.group = lastN[0];
-								serifier.addSequence( nseq );
+							while( n != -1 ) {
+								String sub = nsplit.substring(f+1, n);
+								String ctgname = sub.length() == 1 ? "contig0000"+sub : sub.length() == 2 ? "contig000"+sub : "contig00"+sub;
+								seq = mseq.get(ctgname);
+								touch.add( ctgname );
+								if( seq != null ) {
+									boolean rev = nsplit.charAt(n+1) == '3' ? true : false;
+									Sequence nseq = new Sequence( seq, rev );
+									nseq.setName( ctgname+"_"+(rev?"rev":"frw"));
+									nseq.group = lastN[0];
+									serifier.addSequence( nseq );
+									
+									cseq.append( nseq.sb );
+								} else {
+									System.err.println("errrrror");
+								}
+								
+								f = nsplit.indexOf(')',n+1);
+								n = nsplit.indexOf('_',f+1);
 							}
 						} else {
+							cseq.append("NNN");
 							System.out.println( join(lastN) );
 							//seq = mseq.get(lastW[1]);
 							//if( seq != null ) serifier.addSequence( seq );
+						}
+					} else if( lastW != null && !lastW[0].equals(split[0]) ) {
+						int i = lastW[5].indexOf('0');
+						while( lastW[5].charAt(++i) == '0' ) ;
+						String c1 = lastW[5].substring(i);
+						
+						i = lastW[0].indexOf('0');
+						String sctg = "sctg_"+lastW[0].substring(i+1)+"_000"+next++;
+						Sequence sctgseq = mseq.get(sctg);
+						cseq.append( sctgseq.sb );
+						
+						i = firstofnew[5].indexOf('0');
+						while( firstofnew[5].charAt(++i) == '0' ) ;
+						String c2 = firstofnew[5].substring(i);
+						
+						String nstuff = null;
+						
+						if( mm.containsKey(c1+"_3'") ) {
+							Map<String,String> mog = mm.get(c1+"_3'");
+							nstuff = tengi( mog, c2, 0 );
+							if( mog.containsKey(c2+"_5'") ) {
+								String q = mog.get(c2+"_5'");
+								if( nstuff.length() == 0 ) nstuff = "directconnect("+q+")";
+								else nstuff += " directconnect("+q+")";
+							}
+						}
+						
+						if( nstuff != null && nstuff.length() > 0 ) {
+							String[] nssplit = nstuff.split(" ");
+							int m = 0;
+							String store = "";
+							for( String ns : nssplit ) {
+								int maxmin = 1000;
+								int l = ns.indexOf('(');
+								while( l != -1 ) {
+									int k = ns.indexOf(')',l+1);
+									
+									String nstr = ns.substring(l+1,k);
+									int n = Integer.parseInt( nstr );
+									maxmin = Math.min(maxmin, n);
+									
+									l = ns.indexOf('(',k+1);
+								}
+								
+								if( maxmin > m ) {
+									store = ns;
+									m = maxmin;
+								}
+							}
+							nstuff = store;
+							
+							System.out.println( nstuff );
+							String[] sp = nstuff.split(" ");
+							String nsplit = sp[sp.length-1];
+							int f = nsplit.indexOf(')');
+							int n = nsplit.indexOf('_',f+1);
+							while( n != -1 ) {
+								String sub = nsplit.substring(f+1, n);
+								String ctgname = sub.length() == 1 ? "contig0000"+sub : sub.length() == 2 ? "contig000"+sub : "contig00"+sub;
+								seq = mseq.get(ctgname);
+								touch.add( ctgname );
+								if( seq != null ) {
+									boolean rev = nsplit.charAt(n+1) == '3' ? true : false;
+									Sequence nseq = new Sequence( seq, rev );
+									nseq.setName( ctgname+"_"+(rev?"rev":"frw"));
+									//nseq.group = lastN[0];
+									serifier.addSequence( nseq );
+									cseq.append( nseq.sb );
+								} else {
+									System.err.println("errrrror");
+								}
+								
+								f = nsplit.indexOf(')',n+1);
+								n = nsplit.indexOf('_',f+1);
+							}
 						}
 					}
 					lastN = null;
@@ -198,9 +408,69 @@ public class FlxReader {
 				} else {
 					lastN = split;
 				}
+				
+				if( firstofnew == null || !scaff.equals(firstofnew[0]) ) {
+					next = 1;
+					if( cseq.length() > 0 ) {
+						cseq.setName( cseq.getName() + " ("+cseq.length()+")" );
+						cseq.writeSequence(fw);
+						cseq = new Sequence("plasmid"+plasm++,null);
+					}
+					
+					firstofnew = split;
+				}
+				
 				line = br.readLine();
 			}
 			br.close();
+			fw.close();
+			
+			if( cseq.length() > 0 ) {
+				cseq.setName( cseq.getName() + " ("+cseq.length()+")" );
+				cseq.writeSequence(fw);
+			}
+			
+			if( firstofnew != null ) {
+				int i = lastW[5].indexOf('0');
+				while( lastW[5].charAt(++i) == '0' ) ;
+				String c1 = lastW[5].substring(i);
+				
+				i = firstofnew[5].indexOf('0');
+				while( firstofnew[5].charAt(++i) == '0' ) ;
+				String c2 = firstofnew[5].substring(i);
+				
+				String nstuff = null;
+				if( mm.containsKey(c1+"_3'") ) {
+					Map<String,String> mog = mm.get(c1+"_3'");
+					nstuff = tengi( mog, c2, 0 );
+					if( mog.containsKey(c2+"_5'") ) {
+						String q = mog.get(c2+"_5'");
+						if( nstuff.length() == 0 ) nstuff = "directconnect("+q+")";
+						else nstuff += " directconnect("+q+")";
+					}
+				}
+				
+				if( nstuff != null && nstuff.length() > 0 ) {
+					System.out.println( nstuff );
+					String[] sp = nstuff.split(" ");
+					String nsplit = sp[sp.length-1];
+					int f = nsplit.indexOf(')');
+					int n = nsplit.indexOf('_',f+1);
+					if( n != -1 ) {
+						String sub = nsplit.substring(f+1, n);
+						String ctgname = sub.length() == 1 ? "contig0000"+sub : sub.length() == 2 ? "contig000"+sub : "contig00"+sub;
+						seq = mseq.get(ctgname);
+						touch.add( ctgname );
+						if( seq != null ) {
+							boolean rev = nsplit.charAt(n+1) == '3' ? true : false;
+							Sequence nseq = new Sequence( seq, rev );
+							nseq.setName( ctgname+"_"+(rev?"rev":"frw"));
+							nseq.group = lastN[0];
+							serifier.addSequence( nseq );
+						}
+					}
+				}
+			}
 			
 			for( String seqname : mseq.keySet() ) {
 				if( !touch.contains(seqname) ) {
