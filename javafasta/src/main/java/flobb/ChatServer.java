7ec273706229110e6481f970a1fe6ba35ccf54e7
sigmar <sigmarkarl@gmail.com>
2016-11-02 22:49:10 +0000

    flobb

---
+++ javafasta/src/main/java/flobb/ChatServer.java
@@ -0,0 +1,106 @@
+package flobb;
+
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.WebSocketImpl;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+/**
+ * A simple WebSocketServer implementation. Keeps track of a "chatroom".
+ */
+public class ChatServer extends WebSocketServer {
+	public String message = null;
+	
+	public ChatServer( int port ) throws UnknownHostException {
+		super( new InetSocketAddress( port ) );
+	}
+
+	public ChatServer( InetSocketAddress address ) {
+		super( address );
+	}
+
+	@Override
+	public void onOpen( WebSocket conn, ClientHandshake handshake ) {
+		this.sendToAll( "new connection: " + handshake.getResourceDescriptor() );
+		
+		System.err.println( "bleh " + handshake.getResourceDescriptor() + "  " + handshake.toString() );
+		Iterator<String> it = handshake.iterateHttpFields();
+		while( it.hasNext() ) {
+			String s = it.next();
+			System.err.println( "ok " + s + " " + handshake.getFieldValue( s ) );
+		}
+		//System.out.println( conn.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!" );
+	}
+
+	@Override
+	public void onClose( WebSocket conn, int code, String reason, boolean remote ) {
+		this.sendToAll( conn + " has left the room!" );
+		System.out.println( conn + " has left the room!" );
+	}
+
+	@Override
+	public void onMessage( WebSocket conn, String message ) {
+		//this.sendToAll( message );
+		System.out.println( conn + ": " + message );
+	}
+
+	public static void main( String[] args ) throws InterruptedException , IOException {
+		WebSocketImpl.DEBUG = true;
+		int port = 8887; // 843 flash policy port
+		try {
+			port = Integer.parseInt( args[ 0 ] );
+		} catch ( Exception ex ) {
+		}
+		ChatServer s = new ChatServer( port );
+		s.start();
+		System.out.println( "ChatServer started on port: " + s.getPort() );
+
+		try {
+			Desktop.getDesktop().browse( new URI("http://127.0.0.1:8888/Treedraw.html?ws=127.0.0.1:8887") );
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		
+		BufferedReader sysin = new BufferedReader( new InputStreamReader( System.in ) );
+		while ( true ) {
+			String in = sysin.readLine();
+			s.sendToAll( in );
+		}
+	}
+
+	@Override
+	public void onError( WebSocket conn, Exception ex ) {
+		ex.printStackTrace();
+		if( conn != null ) {
+			// some errors like port binding failed may not be assignable to a specific websocket
+		}
+	}
+
+	/**
+	 * Sends <var>text</var> to all currently connected WebSocket clients.
+	 * 
+	 * @param text
+	 *            The String to send across the network.
+	 * @throws InterruptedException
+	 *             When socket related I/O errors occur.
+	 */
+	public void sendToAll( String text ) {
+		Collection<WebSocket> con = connections();
+		synchronized ( con ) {
+			for( WebSocket c : con ) {
+				c.send( text );
+			}
+		}
+	}
+}
